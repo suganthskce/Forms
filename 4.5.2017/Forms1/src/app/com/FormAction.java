4f2c35321b94079ab6c517f86992dcb2e67b0c8b
suganthskce <suganth2610@gmail.com>
2017-05-04 20:57:26 +0530

    Update FormAction.java

--- 4.5.2017/Forms1/src/app/com/FormAction.java
+++ 4.5.2017/Forms1/src/app/com/FormAction.java
@@ -45,7 +45,36 @@ public class FormAction extends HttpServlet {
 		String query = "insert into tabledetails values('"+tableName+"',";
 		String toTableCreate="";
 		int i;
-		PrintWriter file = new PrintWriter("C:/Users/sugan_000/workspace/Forms1/src/app/com/"+formName+".txt");
+		PrintWriter htmlFile = new PrintWriter("C:/Users/sugan_000/workspace/Forms1/WebContent/"+formName+".html");  // <- Path to create File 
+
+		PrintWriter file = new PrintWriter("C:/Users/sugan_000/workspace/Forms1/src/app/com/"+formName+".txt");  // <- Path to create File 
+		
+		
+		htmlFile.println("<!DOCTYPE html>"
+				 + "<html>"
+				+ "<head>"
+				+ "<meta charset=\"ISO-8859-1\">"
+				+ "<title>"+formName+"</title>"
+				+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+				+ "<link rel=\"stylesheet\" type=\"text/css\" href=\"FormFillPageStylesheet.css\">"
+				+ "</head><body>"
+				+ "<div id=\"nav-div\">"
+				+ "<img alt=\"Form\" id=\"headIco\" src=\"image/form.ico\">"
+				+ "<h1 id=\"headText\" contenteditable=\"false\">"+formName+"</h1>"
+				+ "</div><div>"
+				+ "<form id=\"form\" method=\"post\">"
+				+ "<table id=\"formTable\">");
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		List<String> questionName = new ArrayList<String>();
 		List<String> questionType = new ArrayList<String>();
 		ArrayList options = new ArrayList();
@@ -82,9 +111,7 @@ public class FormAction extends HttpServlet {
 				toTableCreate += ",";
 			}
 		}
-		
 		file.println(questionName.size());
-		
 		Iterator<String> it1 = questionName.iterator();
 		Iterator<String> it2 = questionType.iterator();
 		int pos=0;
@@ -107,6 +134,52 @@ public class FormAction extends HttpServlet {
 			file.println(newString);
 		}
 		
+		//Iterator<String> questionName = questionName.iterator();
+		//Iterator<String> questionTypeTemp = questionType.iterator();
+		int position=0,variable1,variable2;
+		for(variable1=0;variable1<questionName.size();variable1++)) {
+			String questionTypeTemp = questionType.get(variable1);
+			if(questionTypeTemp.equals("shortAnswer")) {
+				htmlFile.println("<tr><td><label id=\"labelStyle\">"+questionName.get(variable1)+"</label></td></tr>");
+				htmlFile.println("<tr><td><input type=\"text\" id=\"option"+(variable1+1)+"\" class=\"shortanswerInput\" placeholder=\"Your Answer\" /></td></tr>");
+			} else if(questionTypeTemp.equals("paragraph")) {
+				htmlFile.println("<tr><td><label id=\"labelStyle\">"+questionName.get(variable1)+"</label></td></tr>");
+				htmlFile.println("<tr><td><input type=\"text\" id=\"option"+(variable1+1)+"\" class=\"paragraphInput\" placeholder=\"Your Answer\" /></td></tr>");
+			} else if(questionTypeTemp.equals("multipleChoice")) {
+				htmlFile.println("<tr><td><label id=\"labelStyle\">"+questionName.get(variable1)+"</label></td></tr>");
+				List opt = (List) options.get(position);
+				htmlFile.println("<tr><td id=\"option"+(variable1+1)+"\">");
+				for(int i=0;i<opt.size();i++)
+				htmlFile.println("<input type=\"radio\" name=\"option"+(variable1+1)+"\" id=\"option"+(variable1+1)+"-"+(i+1)+"\" class=\"radioInput\" value=\""+opt.get(i)+"\"/>"+opt.get(i)+"<br>");
+				htmlFile.println("</td></tr>");
+				position = position+1;
+				
+			} else if(questionTypeTemp.equals("checkbox")) {
+				htmlFile.println("<tr><td><label id=\"labelStyle\">"+questionName.get(variable1)+"</label></td></tr>");
+				List opt = (List) options.get(position);
+				htmlFile.println("<tr><td id=\"option"+(variable1+1)+"\">");
+				for(int i=0;i<opt.size();i++)
+				htmlFile.println("<input type=\"checkbox\" name=\"option"+(variable1+1)+"\" id=\"option"+(variable1+1)+"-"+(i+1)+"\" class=\"checkboxInput\" value=\""+opt.get(i)+"\"/>"+opt.get(i)+"<br>");
+				htmlFile.println("</td></tr>");
+				position = position+1;
+			} else if(questionTypeTemp.equals("dropdown")) {
+				htmlFile.println("<tr><td><label id=\"labelStyle\">"+questionName.get(variable1)+"</label></td></tr>");
+				List opt = (List) options.get(position);
+				htmlFile.println("<tr><td><select id=\"option"+(variable1+1)+"\" name=\"options\" class=\"selectInput\" >");
+				for(int i=0;i<opt.size();i++)
+				htmlFile.println("<option value=\""+opt.get(i)+"\">"+opt.get(i)+"</option>");
+				htmlFile.println("</select></td></tr>");
+				position = position+1;
+			}
+		}
+		htmlFile.println("<tr><td><p id=\"submitBtn\" onclick=\"name()\" >Submit</p></td></tr>"
+				+ "</table>"
+				+ "</form>"
+				+ "</div>"
+				+ "</body>"
+				+ "</html>");	
+			
+			
 		//FileOutputStream fileOut = new FileOutputStream("");
 		/*Iterator itr1 = options.iterator();
 		while(itr1.hasNext()) {
@@ -126,6 +199,7 @@ public class FormAction extends HttpServlet {
 		//System.out.println(questionType);
 	}
 		file.close();
+		htmlFile.close();
 		query+="'localhost:8080/Forms1/OpenFormFillPage?FormName="+formName+"','admin');";
 		try {
 			out.println(toTableCreate);
